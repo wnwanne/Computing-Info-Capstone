47d9b1a18be043d9e496368727029855c6db626b
Winston Nwanne <wnwanne@gmail.com>
2019-09-19 11:26:04 -0400

    new migration script

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
         return '<Post {}'.format(self.body)
